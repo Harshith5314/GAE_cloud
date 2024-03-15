66bf4dacb0ae35271bfae0ff5109534892501bb1
Harshith Kaduluri <kaduluriharshith@gmail.com>
2024-03-15 10:48:28 +0530

    Update Driving.py

--- Driving.py
+++ Driving.py
@@ -1,9 +1,9 @@
 print("Welcome to eligibilty test")
 print("---------------------------")
-name=input("Enter your name ")
-num=input("Enter your age")
+name=input("Enter your name: ")
+num=input("Enter your age: ")
 
 if(int(num)>18):
     print("Your are eligible for driving")
 elif(int(num)<18 or int(num)==18 ):
-    print("your are not eligible for driving")
+    print("you are not eligible for driving")
